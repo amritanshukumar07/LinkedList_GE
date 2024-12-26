f8f823425db6062a538d2d952873afe5bf2143b2
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-26 21:02:03 +0530

    Append method added

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,10 +1,26 @@
 public class LinkedList {
     public Node head;
-
+    public LinkedList(Node head){
+        this.head=head;
+    }
     public void add(int value){
         Node newNode= new Node(value);
         newNode.next=head;
         head= newNode;
+    }
+    public void append(int value){
+        Node newNode= new Node(value);
+         if(head==null){
+            head= newNode;
+         }
+         else{
+             Node temp = head;
+             while(temp.next!=null){
+                 temp= temp.next;
+             }
+             temp.next=newNode;
+
+         }
     }
     public void display(){
         if(head==null){
